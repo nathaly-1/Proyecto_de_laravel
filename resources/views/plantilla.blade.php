076e6bde3696c1b7831cb35c32156e162888227f
nathaly <nathaly.gbm16112021@gmail.com>
2023-06-09 13:02:06 -0600

    version 1.1

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -8,10 +8,10 @@
     <!-- Fontawesome -->
     <script src="https://kit.fontawesome.com/30fc73e490.js" crossorigin="anonymous"></script>
     <!-- pagina -->
-    <link rel="stylesheet" href="{{url ('/')}}/css/plugins/OverlayScrollbars.min.css">
-    <link rel="stylesheet" href="{{url ('/')}}/css/plugins/adminlte.min.css">
-    <link rel="stylesheet" href="{{url ('/')}}/css/css/adminlte.min.css">
-    <link rel="stylesheet" href="{{url ('/')}}/js/plugins/demo.js">
+    <link rel="stylesheet" href="/css/plugins/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="/css/plugins/adminlte.min.css">
+    <link rel="stylesheet" href="/css/css/adminlte.min.css">
+    <link rel="stylesheet" href="/js/plugins/demo.js">
     <!-- bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
@@ -31,8 +31,8 @@
             integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g=="
             crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    <script src="{{ url ('/')}}/js/plugins/jquery.overlayScrollbars.min.js"></script>
-    <script src="{{ url ('/')}}/js/plugins/adminlte.js"></script>
+    <script src="/js/plugins/jquery.overlayScrollbars.min.js"></script>
+    <script src="/js/plugins/adminlte.js"></script>
 
 </head>
 <body class="hold-transition sidebar-mini">
